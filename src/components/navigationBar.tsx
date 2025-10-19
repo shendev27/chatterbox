73e73225966e8bd3565f8180ce327c28170b66f4
Ostine Chen <chenostine123@gmail.com>
2025-10-19 15:54:48 -0400

    temp login added for nav bar

--- src/components/navigationBar.tsx
+++ src/components/navigationBar.tsx
@@ -12,6 +12,7 @@ import {
 
 export default function NavigationBar() {
   return (
+    <>
     <div className="w-full p-4 bg-white shadow-sm ">
       <NavigationMenu>
         <NavigationMenuList>
@@ -42,5 +43,33 @@ export default function NavigationBar() {
         </NavigationMenuList>
       </NavigationMenu>
     </div>
+    <div className="w-full p-4 bg-white shadow-sm ">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger className="text-lg font-medium text-center">
+              Sign in
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <div className="flex flex-col w-48 p-2">
+                <Link href="/join-room">
+                  <NavigationMenuLink className="block px-4 py-3 rounded-md hover:bg-gray-100 transition-colors duration-200 text-gray-700 hover:text-gray-900">
+                    Login
+                  </NavigationMenuLink>
+                </Link>
+                <Link href="/create-room">
+                  <NavigationMenuLink className="block px-4 py-3 rounded-md hover:bg-gray-100 transition-colors duration-200 text-gray-700 hover:text-gray-900">
+                    Create Account
+                  </NavigationMenuLink>
+                </Link>
+              </div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+    </>
+    
+    
   );
 }
